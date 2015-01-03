dc671ad7239de6b6e1eed668464b866931b6f5bf
Ryan Gonzalez <rymg19@gmail.com>
2015-01-03 14:16:15 -0600

    Update year

--- tinymk.py
+++ tinymk.py
@@ -1,7 +1,7 @@
 # TinyMk
 # A miniature Makefile alternative
 #
-# Copyright (c) 2014 Ryan Gonzalez
+# Copyright (c) 2015 Ryan Gonzalez
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
